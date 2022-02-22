7dd5f61725352f750dc8d8861553e32334026efb
Julia Guerreiro <juguerreiro123@gmail.com>
2022-02-22 14:52:31 -0300

    add 2 tasks at the root and make it as link

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,6 +11,7 @@ class TasksController < ApplicationController
   end
 
   def show
+
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "tasks/new", to: "tasks#new" # pegar a informaçao do usuario
 
   post "tasks", to: "tasks#create" # mandar a informacao para o DB
-  get "tasks/:id", to: "tasks#show" # mostrar a informacao de apenas um task pelo seu id
+  get "tasks/:id", to: "tasks#show", as: :task # mostrar a informacao de apenas um task pelo seu id
 
   get "tasks/:id/edit", to: "tasks#edit" # editar a informacao de um task pelo seu id
   patch "tasks/:id", to: "tasks#update" # enviar essa informacao de atualizacao para o DB
