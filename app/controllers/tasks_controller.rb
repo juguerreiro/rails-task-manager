87596d5a1ccedf315fed0e985aebcfca5648ed69
Julia Guerreiro <juguerreiro123@gmail.com>
2022-02-22 16:17:25 -0300

    create a private method to create a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,13 +5,16 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
   end
 
   def show
-
+    @task = Task.find(params[:id])
   end
 
   def edit
@@ -21,5 +24,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
